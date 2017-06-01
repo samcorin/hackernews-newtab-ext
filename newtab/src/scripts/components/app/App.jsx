032581686c2ae80ae7211d0003ca59f20d12e2aa
Sam Corin <samcorin@gmail.com>
2017-06-01 19:09:55 +0900

    removed old code

--- newtab/src/scripts/components/app/App.jsx
+++ newtab/src/scripts/components/app/App.jsx
@@ -1,81 +1,35 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-// import axios from 'axios';
-// import Moment from 'react-moment';
-// import store from './../../index';
+// import moment from 'moment';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     data: this.props.data
-  //   };
-  // }
-
-    // store.subscribe(() => {
-    //   // When state will be updated(in our case, when items will be fetched), we will update local component state and force component to rerender with new data.
-    //   this.setState({
-    //     // data: store.getState().data;
-    //     data: this.props.data
-    //   });
-    // });
-  // }
-
-  // componentDidMount() {
-  //   console.log("props data: ", this.props.data)
-  //   // state.dispatch(fetchData());
-  // }
-
   render() {
-    // console.log("Props:", this.props.data);
-    // console.log("state data: ", this.props)
-    // fix this
     if (this.props.data) {
-
-      // need to only put in the datsa into array, not surrounding obj
-      // console.log("OK")
-      // console.log(this.props.data)
-      // console.log(this.props.data[1])
-      // this.props.data.forEach((item) => {
-      //   console.log("item: ",item)
-      // })
-      // console.log("mapping...")
-      // this.props.data.map((item, index) => {
-      //   console.log(item.data);
-      // });
-
-      // console.log("Props.data: ", this.props.data)
-      // return (
-        var stories = this.props.data.map((item, index) => {
-          // console.log("index");
-          // console.log(this.props.data)
-          // console.log(item)
-
-          return (
-            <li key={index} className="item">
-              <div className="title-line">
-                <a href={item.data.url} target="_blank">{item.data.title}</a> <span className="comm-url"> (somewhere.com) </span>
-              </div>
-              <div className="details-line">
-                <span className="score">{item.data.score} points</span> by <span className="username"> {item.data.by} </span>
-                <span className="age"></span> |
-                <span className="comments"> {item.descendants} comments</span>
-              </div>
-            </li>
-          )
-        });
-
+      var stories = this.props.data.map((item, index) => {
         return (
-          <div className="flexbox">
-            <ul className="el">{stories}</ul>
-            <div className="el">hello hello</div>
-          </div>
-        );
-      // )
-    } else {
+          <li key={index} className="item">
+            <div className="title-line">
+              <a href={item.data.url} target="_blank">{item.data.title}</a> <span className="comm-url"> (somewhere.com) </span>
+            </div>
+            <div className="details-line">
+              <span className="score">{item.data.score} points</span> by <span className="username"> {item.data.by} </span>
+              <span className="age"></span> |
+              <span className="comments"> {item.descendants} comments</span>
+            </div>
+          </li>
+        )
+      });
+
       return (
-        <div>error</div>
+        <div className="flexbox">
+          <ul className="el">{stories}</ul>
+        </div>
+      );
+
+    } else {
+
+      return (
+        <div>There was an error :/</div>
       )
     }
   }
@@ -88,7 +42,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-// export default App;
 export default connect(mapStateToProps)(App);
-
-
